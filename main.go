4cf74dcd2e252b1e3a7a339641ea7cee1e72aff0
ciusji <bqjimaster@gmail.com>
2022-08-10 09:15:48 +0800

    add pkg and delete browser

--- main.go
+++ main.go
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-package main // import "github.com/minio/minio"
+package main // import "github.com/GuinsooLab/annastore"
 
 import (
 	"os"
